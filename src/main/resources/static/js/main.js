b9fd79f5a2babbda8d87c47a2aa72fe85552b674
rerekovich <rerekovich1337@gmail.com>
2025-06-03 05:49:20 +0300

    link

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,4 +1,4 @@
-const api = axios.create({ baseURL: 'http://localhost:8080/api' });
+const api = axios.create({ baseURL: 'https://computerstore-cci2.onrender.com/api' });
 const categories = [
   "DESKTOP", "MONITOR", "LAPTOP", "TABLET", "PRINTER",
   "MEMORY_DEVICE", "PROCESSOR", "POWER_SUPPLY", "INPUT_DEVICE"
